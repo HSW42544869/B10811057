ee75e4b86a06a912a2b885998a48769e64ca33fb
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-10-04 15:44:18 +0800

    更新

--- B10811057/Site1.Master.designer.cs
+++ B10811057/Site1.Master.designer.cs
@@ -42,22 +42,13 @@ namespace B10811057
         protected global::System.Web.UI.WebControls.Image Image1;
 
         /// <summary>
-        /// HyperLink1 控制項。
+        /// Image2 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
-
-        /// <summary>
-        /// HyperLink2 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        protected global::System.Web.UI.WebControls.Image Image2;
 
         /// <summary>
         /// ContentPlaceHolder1 控制項。
