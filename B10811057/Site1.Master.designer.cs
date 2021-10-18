cc4bbdf3a17a5f8cd5b11e9b3ccef871b21931e9
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-10-18 13:45:39 +0800

    更新

--- B10811057/Site1.Master.designer.cs
+++ B10811057/Site1.Master.designer.cs
@@ -24,6 +24,15 @@ namespace B10811057
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// ContentPlaceHolder2 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
         /// form1 控制項。
         /// </summary>
         /// <remarks>
@@ -31,6 +40,15 @@ namespace B10811057
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// MainContent 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
 
         /// <summary>
         /// Image1 控制項。
